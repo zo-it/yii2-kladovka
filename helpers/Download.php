bc88add02ca97bed59ab707d0aaf7c1ffc90059e
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-05-09 23:54:35 +0400

    add timeout()

--- helpers/Download.php
+++ helpers/Download.php
@@ -8,19 +8,45 @@ class Download
 
     private $_url = null;
 
-    public function url($url)
+    public function setUrl($url)
     {
         $this->_url = $url;
         return $this;
     }
 
-    public function setUrl($url)
-    {
-        return $this->url($url);
-    }
-
     public function getUrl()
     {
         return $this->_url;
+    }
+
+    public function url($url = null)
+    {
+        if (!is_null($url)) {
+            return $this->setUrl($url);
+        } else {
+            return $this->getUrl();
+        }
+    }
+
+    private $_timeout = 5;
+
+    public function setTimeout($timeout)
+    {
+        $this->_timeout = $timeout;
+        return $this;
+    }
+
+    public function getTimeout()
+    {
+        return $this->_timeout;
+    }
+
+    public function timeout($timeout = null)
+    {
+        if (!is_null($timeout)) {
+            return $this->setTimeout($timeout);
+        } else {
+            return $this->getTimeout();
+        }
     }
 }
