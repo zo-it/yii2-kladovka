6ad9bf200b707ecda59d29cff1f7c232c9aa1b9d
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-05-15 07:22:33 +0400

    fix getResult(), getErrno(), getError(), getInfo()

--- net/Curl.php
+++ net/Curl.php
@@ -1072,11 +1072,7 @@ class Curl
 
     public function getResult()
     {
-        if (is_string($this->_result) || is_bool($this->_result)) {
-            return $this->_result;
-        } else {
-            throw new \Exception('Unable to return a result.');
-        }
+        return $this->_result;
     }
 
     public function result()
@@ -1094,11 +1090,7 @@ class Curl
 
     public function getErrno()
     {
-        if (is_int($this->_errno)) {
-            return $this->_errno;
-        } else {
-            throw new \Exception('Unable to return an error number.');
-        }
+        return $this->_errno;
     }
 
     public function errno()
@@ -1116,11 +1108,7 @@ class Curl
 
     public function getError()
     {
-        if (is_string($this->_error)) {
-            return $this->_error;
-        } else {
-            throw new \Exception('Unable to return an error message.');
-        }
+        return $this->_error;
     }
 
     public function error()
@@ -1138,14 +1126,10 @@ class Curl
 
     public function getInfo($key = null)
     {
-        if (is_array($this->_info)) {
-            if ($key && is_string($key)) {
-                return array_key_exists($key, $this->_info) ? $this->_info[$key] : null;
-            } else {
-                return $this->_info;
-            }
+        if ($key && is_string($key)) {
+            return (is_array($this->_info) && array_key_exists($key, $this->_info)) ? $this->_info[$key] : null;
         } else {
-            throw new \Exception('Unable to return an info.');
+            return $this->_info;
         }
     }
 
