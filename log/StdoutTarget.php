5a8e24789fc51b6d0c61e2e65e3f9bc80e4346ee
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-16 11:25:23 +0400

    fix log/StdoutTarget.php

--- log/StdoutTarget.php
+++ log/StdoutTarget.php
@@ -3,16 +3,38 @@
 namespace yii\kladovka\log;
 
 use yii\log\Target,
+    yii\log\Logger,
     yii\helpers\Console;
 
 
 class StdoutTarget extends Target
 {
 
+    private $_stdoutSupportsAnsiColors = false;
+
+    public function init()
+    {
+        parent::init();
+        $this->_stdoutSupportsAnsiColors = Console::streamSupportsAnsiColors(\STDOUT);
+    }
+
     public function export()
     {
+        static $levelFormatMap = [
+            Logger::LEVEL_ERROR => [Console::FG_RED],
+            Logger::LEVEL_WARNING => [Console::FG_YELLOW],
+            Logger::LEVEL_INFO => [],
+            Logger::LEVEL_TRACE => [Console::FG_CYAN],
+            Logger::LEVEL_PROFILE => [Console::FG_PURPLE],
+            Logger::LEVEL_PROFILE_BEGIN => [Console::FG_PURPLE],
+            Logger::LEVEL_PROFILE_END => [Console::FG_PURPLE]
+        ];
         foreach ($this->messages as $message) {
-            Console::stdout($this->formatMessage($message) . "\n");
+            $string = $this->formatMessage($message) . "\n";
+            if ($this->_stdoutSupportsAnsiColors && array_key_exists($message[1], $levelFormatMap)) {
+                $string = Console::ansiFormat($string, $levelFormatMap[$message[1]]);
+            }
+            Console::stdout($string);
         }
     }
 }
