d6765451ed72907f7c94752b1f8c2acf245c1ab2
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-16 12:10:08 +0400

    add stderr

--- log/StdoutTarget.php
+++ log/StdoutTarget.php
@@ -10,31 +10,51 @@ use yii\log\Target,
 class StdoutTarget extends Target
 {
 
-    private $_stdoutSupportsAnsiColors = false;
+    private $_stdoutIsTerminal = false;
+
+    private $_stdoutSupportsColors = false;
+
+    private $_stderrIsTerminal = false;
+
+    private $_stderrSupportsColors = false;
+
+    private $_levelFormatMap = [
+        Logger::LEVEL_ERROR => [Console::BOLD, Console::FG_RED],
+        Logger::LEVEL_WARNING => [Console::BOLD, Console::FG_YELLOW],
+        Logger::LEVEL_INFO => [],
+        Logger::LEVEL_TRACE => [Console::FG_CYAN],
+        Logger::LEVEL_PROFILE => [Console::FG_PURPLE],
+        Logger::LEVEL_PROFILE_BEGIN => [Console::FG_PURPLE],
+        Logger::LEVEL_PROFILE_END => [Console::FG_PURPLE]
+    ];
 
     public function init()
     {
         parent::init();
-        $this->_stdoutSupportsAnsiColors = Console::streamSupportsAnsiColors(\STDOUT);
+        $this->_stdoutIsTerminal = posix_isatty(\STDOUT);
+        $this->_stdoutSupportsColors = Console::streamSupportsAnsiColors(\STDOUT);
+        $this->_stderrIsTerminal = posix_isatty(\STDERR);
+        $this->_stderrSupportsColors = Console::streamSupportsAnsiColors(\STDERR);
     }
 
     public function export()
     {
-        static $levelFormatMap = [
-            Logger::LEVEL_ERROR => [Console::BOLD, Console::FG_RED],
-            Logger::LEVEL_WARNING => [Console::BOLD, Console::FG_YELLOW],
-            Logger::LEVEL_INFO => [],
-            Logger::LEVEL_TRACE => [Console::FG_CYAN],
-            Logger::LEVEL_PROFILE => [Console::FG_PURPLE],
-            Logger::LEVEL_PROFILE_BEGIN => [Console::FG_PURPLE],
-            Logger::LEVEL_PROFILE_END => [Console::FG_PURPLE]
-        ];
         foreach ($this->messages as $message) {
             $string = $this->formatMessage($message) . "\n";
-            if ($this->_stdoutSupportsAnsiColors && array_key_exists($message[1], $levelFormatMap)) {
-                $string = Console::ansiFormat($string, $levelFormatMap[$message[1]]);
+            $level = $message[1];
+            $format = array_key_exists($level, $this->_levelFormatMap) ? $this->_levelFormatMap[$level] : [];
+            if ($this->_stdoutIsTerminal && $this->_stdoutSupportsColors) {
+                Console::stdout(Console::ansiFormat($string, $format));
+            } else {
+                Console::stdout($string);
             }
-            Console::stdout($string);
+            if (($level == Logger::LEVEL_ERROR || $level == Logger::LEVEL_WARNING) && !$this->_stdoutIsTerminal && $this->_stderrIsTerminal) {
+                if ($this->_stderrSupportsColors) {
+                    Console::stderr(Console::ansiFormat($string, $format));
+                } else {
+                    Console::stderr($string);
+                }
+            }
         }
     }
 }
