87d7194b9ea5d69bb7e99461958a6551514724da
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-10-25 02:08:03 +0400

    fix TimestampBehavior

--- behaviors/TimestampBehavior.php
+++ behaviors/TimestampBehavior.php
@@ -20,44 +20,40 @@ class TimestampBehavior extends Behavior
     public function events()
     {
         return [
-            ActiveRecord::EVENT_BEFORE_VALIDATE => 'beforeSave',
-            ActiveRecord::EVENT_AFTER_INSERT => 'afterFind',
-            ActiveRecord::EVENT_AFTER_UPDATE => 'afterFind',
-            ActiveRecord::EVENT_AFTER_FIND => 'afterFind'
+            ActiveRecord::EVENT_BEFORE_VALIDATE => 'encodeData',
+            ActiveRecord::EVENT_AFTER_INSERT => 'decodeData',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'decodeData',
+            ActiveRecord::EVENT_AFTER_FIND => 'decodeData'
         ];
     }
 
-    public function beforeSave($event)
+    public function encodeData($event)
     {
         $owner = $this->owner;
         if ($owner instanceof ActiveRecord) {
             $timestampAttribute = $this->timestampAttribute;
-            if ($timestampAttribute && is_string($timestampAttribute)) {
-                if ($owner->hasAttribute($timestampAttribute)) {
-                    switch ($owner->getTableSchema()->getColumn($timestampAttribute)->dbType) {
-                        case 'date': $format = $this->dateFormat; break;
-                        case 'time': $format = $this->timeFormat; break;
-                        default: $format = $this->dateTimeFormat; break;
-                    }
-                    $owner->{$timestampAttribute} = date($format);
+            if ($timestampAttribute && is_string($timestampAttribute) && $owner->hasAttribute($timestampAttribute)) {
+                switch ($owner->getTableSchema()->getColumn($timestampAttribute)->dbType) {
+                    case 'date': $format = $this->dateFormat; break;
+                    case 'time': $format = $this->timeFormat; break;
+                    default: $format = $this->dateTimeFormat; break;
                 }
+                $owner->{$timestampAttribute} = date($format);
             }
         }
     }
 
-    public function afterFind($event)
+    public function decodeData($event)
     {
         $owner = $this->owner;
         if ($owner instanceof ActiveRecord) {
             $timestampAttribute = $this->timestampAttribute;
-            if ($timestampAttribute && is_string($timestampAttribute)) {
-                if ($owner->hasAttribute($timestampAttribute)) {
-                    if ($owner->{$timestampAttribute} && is_string($owner->{$timestampAttribute})) {
-                        if (($owner->{$timestampAttribute} == '0000-00-00') || ($owner->{$timestampAttribute} == '00:00:00') || ($owner->{$timestampAttribute} == '0000-00-00 00:00:00')) {
-                            $owner->{$timestampAttribute} = 0;
-                        } else {
-                            $owner->{$timestampAttribute} = strtotime($owner->{$timestampAttribute});
-                        }
+            if ($timestampAttribute && is_string($timestampAttribute) && $owner->hasAttribute($timestampAttribute)) {
+                if ($owner->{$timestampAttribute} && is_string($owner->{$timestampAttribute})) {
+                    if (($owner->{$timestampAttribute} == '0000-00-00') || ($owner->{$timestampAttribute} == '00:00:00') || ($owner->{$timestampAttribute} == '0000-00-00 00:00:00')) {
+                        $owner->{$timestampAttribute} = 0;
+                    } else {
+                        $owner->{$timestampAttribute} = strtotime($owner->{$timestampAttribute});
                     }
                 }
             }
