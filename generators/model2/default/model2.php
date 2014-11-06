629ca2a1c4b4b6cebd20c196c65ba7efc61da8ef
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-06 23:46:34 +0300

    fix model

--- generators/model2/default/model2.php
+++ generators/model2/default/model2.php
@@ -11,9 +11,7 @@ $secondModelClass = StringHelper::basename($generator->secondModelClass);
 $modelNamespace = StringHelper::dirname(ltrim($generator->modelClass, '\\'));
 $secondModelNamespace = StringHelper::dirname(ltrim($generator->secondModelClass, '\\'));
 
-$baseQueryClass = Yii::$app->hasModule('mozayka') ? 'yii\mozayka\db\ActiveQuery' : 'yii\kladovka\db\ActiveQuery';
-$use = [$baseQueryClass];
-
+$use = [];
 $modelAlias = $modelClass;
 if ($modelNamespace != $secondModelNamespace) {
     if ($modelClass == $secondModelClass) {
@@ -23,7 +21,10 @@ if ($modelNamespace != $secondModelNamespace) {
         $use[] = $modelNamespace . '\\' . $modelClass;
     }
 }
-
+if ($secondModelClass == 'User') {
+    $use[] = 'yii\web\IdentityInterface';
+}
+$use[] = Yii::$app->hasModule('mozayka') ? 'yii\mozayka\db\ActiveQuery' : 'yii\kladovka\db\ActiveQuery';
 $use[] = 'Yii';
 
 echo "<?php\n";
@@ -36,14 +37,55 @@ use <?php echo implode(",\n    ", $use); ?>;
 <?php } ?>
 
 
+<?php if ($secondModelClass == 'User') { ?>
+class <?php echo $secondModelClass; ?> extends <?php echo $modelAlias; ?> implements IdentityInterface
+<?php } else { ?>
 class <?php echo $secondModelClass; ?> extends <?php echo $modelAlias; ?>
 
+<?php } ?>
 {
 
     public static function find()
     {
         return Yii::createObject(<?php echo $secondModelClass; ?>Query::className(), [get_called_class()]);
     }
+<?php if ($secondModelClass == 'User') { ?>
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($accessToken, $type = null)
+    {
+        return static::findOne(['access_token' => $accessToken]);
+    }
+
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username]);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->auth_key == $authKey;
+    }
+
+    public function validatePassword($password)
+    {
+        return $this->password == $password;
+    }
+<?php } ?>
 
     public function behaviors()
     {
