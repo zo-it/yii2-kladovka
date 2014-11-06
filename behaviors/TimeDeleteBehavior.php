fa69629bb5c9f03a55a32714f48c5d8baf750790
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-07 01:01:52 +0300

    fix behaviors

--- behaviors/TimeDeleteBehavior.php
+++ behaviors/TimeDeleteBehavior.php
@@ -21,11 +21,14 @@ class TimeDeleteBehavior extends Behavior
     public function events()
     {
         return [
-            ActiveRecord::EVENT_BEFORE_DELETE => 'invalidEvent',
             ActiveRecord::EVENT_BEFORE_VALIDATE => 'encodeData',
+            ActiveRecord::EVENT_AFTER_VALIDATE => 'decodeData',
+            ActiveRecord::EVENT_BEFORE_INSERT => 'encodeData',
             ActiveRecord::EVENT_AFTER_INSERT => 'decodeData',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'encodeData',
             ActiveRecord::EVENT_AFTER_UPDATE => 'decodeData',
-            ActiveRecord::EVENT_AFTER_FIND => 'decodeData'
+            ActiveRecord::EVENT_AFTER_FIND => 'decodeData',
+            ActiveRecord::EVENT_BEFORE_DELETE => 'invalidEvent'
         ];
     }
 
