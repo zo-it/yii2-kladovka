a9b6299c1e728b11b0400055ac2f18179e3f3cea
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-11-07 23:15:37 +0300

    fix bootstrap()

--- Module.php
+++ Module.php
@@ -5,7 +5,8 @@ namespace yii\kladovka;
 use yii\base\Module as YiiModule,
     yii\base\BootstrapInterface,
     yii\web\Application as YiiWebApplication,
-    yii\console\Application as YiiConsoleApplication;
+    yii\console\Application as YiiConsoleApplication,
+    Yii;
 
 
 class Module extends YiiModule implements BootstrapInterface
@@ -19,8 +20,14 @@ class Module extends YiiModule implements BootstrapInterface
                 'sourceLanguage' => 'en-US',
                 'basePath' => '@yii/kladovka/messages'
             ];
-        } elseif ($app instanceof YiiConsoleApplication) {
-            if (YII_ENV_DEV) {
+        } elseif (YII_ENV_DEV && ($app instanceof YiiConsoleApplication)) {
+            $gii = Yii::$app->getModule('gii');
+            if ($gii) {
+                $gii->generators = array_merge($gii->generators, [
+                    'model2' => 'yii\kladovka\generators\model2\Generator',
+                    'search' => 'yii\kladovka\generators\search\Generator',
+                    'search2' => 'yii\kladovka\generators\search2\Generator'
+                ]);
                 $app->controllerMap[$this->id] = 'yii\kladovka\console\GenerateController';
             }
         }
