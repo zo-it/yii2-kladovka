7d54f251a20cff2a0a2c263e7243fea9b2c0ae78
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-12-06 23:03:33 +0300

    fix search2/Generator

--- generators/search2/Generator.php
+++ generators/search2/Generator.php
@@ -2,55 +2,22 @@
 
 namespace yii\kladovka\generators\search2;
 
-use yii\gii\generators\crud\Generator as GiiCrudGenerator,
+use yii\kladovka\generators\model2\Generator as Model2Generator,
     yii\gii\CodeFile,
     Yii;
 
 
-class Generator extends GiiCrudGenerator
+class Generator extends Model2Generator
 {
 
-    public $secondModelClass;
+    public $templates = [
+        'log' => '@yii/kladovka/generators/search2/log',
+        'user' => '@yii/kladovka/generators/search2/user'
+    ];
 
     public function getName()
     {
         return 'Search Generator 2';
-    }
-
-    public function getDescription()
-    {
-        return '';
-    }
-
-    public function attributes()
-    {
-        $attributes = array_diff(parent::attributes(), ['controllerClass', 'baseControllerClass', 'indexWidgetType']);
-        $key = array_search('searchModelClass', $attributes);
-        if ($key !== false) {
-            $attributes[$key] = 'secondModelClass';
-        }
-        return $attributes;
-    }
-
-    public function rules()
-    {
-        $rules = [];
-        foreach (parent::rules() as $rule) {
-            $rule[0] = array_diff($rule[0], ['controllerClass', 'baseControllerClass', 'indexWidgetType']);
-            if ($rule[0]) {
-                $key = array_search('searchModelClass', $rule[0]);
-                if ($key !== false) {
-                    $rule[0][$key] = 'secondModelClass';
-                }
-                $rules[] = $rule;
-            }
-        }
-        $rules[] = [['secondModelClass'], 'required'];
-        return $rules;
-    }
-
-    public function validateModelClass()
-    {
     }
 
     public function requiredTemplates()
--- generators/search2/default/search2.php
+++ generators/search2/default/search2.php
@@ -1,102 +1,27 @@
 <?php
-use yii\helpers\StringHelper;
 /**
  * @var yii\web\View $this
  * @var yii\kladovka\generators\search2\Generator $generator
  */
 
-$modelClass = StringHelper::basename($generator->modelClass);
-$secondModelClass = StringHelper::basename($generator->secondModelClass);
-
-$modelNamespace = StringHelper::dirname(ltrim($generator->modelClass, '\\'));
-$secondModelNamespace = StringHelper::dirname(ltrim($generator->secondModelClass, '\\'));
-
-$use = [];
-$modelAlias = $modelClass;
-if ($modelNamespace != $secondModelNamespace) {
-    if ($modelClass == $secondModelClass) {
-        $modelAlias .= 'Model';
-        $use[] = $modelNamespace . '\\' . $modelClass . ' as ' . $modelAlias;
-    } else {
-        $use[] = $modelNamespace . '\\' . $modelClass;
-    }
-}
-$use[] = 'yii\base\Model';
-
-$behaviors = [];
-foreach ($generator->getTableSchema()->columns as $columnSchema) {
-    if (in_array($columnSchema->type, ['datetime', 'date', 'time'])) {
-        if (!array_key_exists('datetime', $behaviors)) {
-            $behaviors['datetime'] = [
-                'class' => 'yii\kladovka\behaviors\DatetimeBehavior',
-                'attributes' => [$columnSchema->name]
-            ];
-        } else {
-            $behaviors['datetime']['attributes'][] = $columnSchema->name;
-        }
-    }
-    if ($columnSchema->allowNull) {
-        if (!array_key_exists('nullable', $behaviors)) {
-            $behaviors['nullable'] = [
-                'class' => 'yii\kladovka\behaviors\NullableBehavior',
-                'attributes' => [$columnSchema->name]
-            ];
-        } else {
-            $behaviors['nullable']['attributes'][] = $columnSchema->name;
-        }
-    }
-}
-if ($secondModelClass == 'LogSearch') {
-    $behaviors['datetime'] = [
-        'class' => 'yii\kladovka\behaviors\DatetimeBehavior',
-        'attributes' => ['log_time']
-    ];
-}
+$use = $generator->prepareUse(['yii\base\Model']);
+$behaviors = $generator->prepareBehaviors();
 
 echo "<?php\n";
 ?>
 
-namespace <?php echo $secondModelNamespace; ?>;
+namespace <?php echo $generator->getSecondModelNamespace(); ?>;
 <?php if ($use) { ?>
 
 use <?php echo implode(",\n    ", $use); ?>;
 <?php } ?>
 
 
-class <?php echo $secondModelClass; ?> extends <?php echo $modelAlias; ?>
+class <?php echo $generator->getSecondModelName(); ?> extends <?php echo $generator->getModelAlias(); ?>
 
 {
-<?php /*if ($behaviors) {*/ ?>
 
-    public function behaviors()
-    {
-        return [
-<?php
-foreach (array_values($behaviors) as $i => $behavior) {
-    if (is_string($behavior)) {
-        echo '            \'' . $behavior . '\'' . (($i < count($behaviors) - 1) ? ",\n" : "\n");
-    } elseif (is_array($behavior)) {
-        $behaviorKeys = array_keys($behavior);
-        $behaviorValues = array_values($behavior);
-        if (count($behavior) == 1) {
-            echo '            [\'' . $behaviorKeys[0] . '\' => \'' . $behaviorValues[0] . '\']' . (($i < count($behaviors) - 1) ? ",\n" : "\n");
-        } else {
-            echo '            [' . "\n";
-            foreach ($behaviorValues as $j => $behaviorValue) {
-                if (is_string($behaviorValue)) {
-                    echo '                \'' . $behaviorKeys[$j] . '\' => \'' . $behaviorValue . '\'' . (($j < count($behavior) - 1) ? ",\n" : "\n");
-                } elseif (is_array($behaviorValue)) {
-                    echo '                \'' . $behaviorKeys[$j] . '\' => [\'' . implode('\', \'', $behaviorValue) . '\']' . (($j < count($behavior) - 1) ? ",\n" : "\n");
-                }
-            }
-            echo '            ]' . (($i < count($behaviors) - 1) ? ",\n" : "\n");
-        }
-    }
-}
-?>
-        ];
-    }
-<?php /*}*/ ?>
+<?php echo $generator->renderBehaviors($behaviors); ?>
 
     public function beforeValidate()
     {
