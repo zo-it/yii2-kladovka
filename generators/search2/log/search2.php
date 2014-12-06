12b846467a04c65edc9d168145418cfe7e69fe0e
Ivan Chistyakov <ivan.chkv@gmail.com>
2014-12-06 23:14:59 +0300

    fix search2

---
+++ generators/search2/log/search2.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @var yii\web\View $this
+ * @var yii\kladovka\generators\search2\Generator $generator
+ */
+
+$use = $generator->prepareUse(['yii\base\Model']);
+$behaviors = $generator->prepareBehaviors();
+$behaviors['datetime'] = [
+    'class' => 'yii\kladovka\behaviors\DatetimeBehavior',
+    'attributes' => ['log_time']
+];
+
+echo "<?php\n";
+?>
+
+namespace <?php echo $generator->getSecondModelNamespace(); ?>;
+<?php if ($use) { ?>
+
+use <?php echo implode(",\n    ", $use); ?>;
+<?php } ?>
+
+
+class <?php echo $generator->getSecondModelName(); ?> extends <?php echo $generator->getModelAlias(); ?>
+
+{
+
+<?php echo $generator->renderBehaviors($behaviors); ?>
+
+    public function beforeValidate()
+    {
+        return Model::beforeValidate();
+    }
+
+    public function afterValidate()
+    {
+        Model::afterValidate();
+    }
+}
---
+++ generators/search2/user/search2.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @var yii\web\View $this
+ * @var yii\kladovka\generators\search2\Generator $generator
+ */
+
+$use = $generator->prepareUse(['yii\base\Model']);
+$behaviors = $generator->prepareBehaviors();
+
+echo "<?php\n";
+?>
+
+namespace <?php echo $generator->getSecondModelNamespace(); ?>;
+<?php if ($use) { ?>
+
+use <?php echo implode(",\n    ", $use); ?>;
+<?php } ?>
+
+
+class <?php echo $generator->getSecondModelName(); ?> extends <?php echo $generator->getModelAlias(); ?>
+
+{
+
+<?php echo $generator->renderBehaviors($behaviors); ?>
+
+    public function beforeValidate()
+    {
+        return Model::beforeValidate();
+    }
+
+    public function afterValidate()
+    {
+        Model::afterValidate();
+    }
+}
