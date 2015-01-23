38e8b83ff3542d7c3784c06606da1ac0f0473c72
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-01-23 12:38:06 +0300

    add dump(), dumpAsString(), log()

--- db/ActiveRecord.php
+++ db/ActiveRecord.php
@@ -3,6 +3,7 @@
 namespace yii\kladovka\db;
 
 use yii\db\ActiveRecord as YiiActiveRecord,
+    yii\helpers\VarDumper,
     Yii;
 
 
@@ -12,5 +13,55 @@ class ActiveRecord extends YiiActiveRecord
     public static function find()
     {
         return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
+    }
+
+    public static function dump()
+    {
+        if ($this->hasErrors()) {
+            VarDumper::dump([
+                'class' => get_class($this),
+                'attributes' => $this->getAttributes(),
+                'errors' => $this->getErrors()
+            ]);
+        } else {
+            VarDumper::dump([
+                'class' => get_class($this),
+                'attributes' => $this->getAttributes()
+            ]);
+        }
+    }
+
+    public static function dumpAsString()
+    {
+        if ($this->hasErrors()) {
+            return VarDumper::dumpAsString([
+                'class' => get_class($this),
+                'attributes' => $this->getAttributes(),
+                'errors' => $this->getErrors()
+            ]);
+        } else {
+            return VarDumper::dumpAsString([
+                'class' => get_class($this),
+                'attributes' => $this->getAttributes()
+            ]);
+        }
+    }
+
+    public static function log($message = 'No message.', $category = 'application')
+    {
+        if ($this->hasErrors()) {
+            Yii::error(VarDumper::dumpAsString([
+                'message' => $message,
+                'class' => get_class($this),
+                'attributes' => $this->getAttributes(),
+                'errors' => $this->getErrors()
+            ]), $category);
+        } else {
+            Yii::info(VarDumper::dumpAsString([
+                'message' => $message,
+                'class' => get_class($this),
+                'attributes' => $this->getAttributes()
+            ]), $category);
+        }
     }
 }
